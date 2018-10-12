21a42ec947be4c9088da81ff285da6fc309d0e2e
Chieh-Chun Chang <say543@msn.com>
2018-10-12 16:45:49 -0700

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,126 @@
+
+# install matplotlib
+#import matplotlib.pyplot as plt
+
+import numpy as np
+
+# install scikit-learn
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+np.random.seed(123)
+
+# this does not work. skip it at first
+# https://blog.csdn.net/liangzuojiayi/article/details/78183783
+# https://ipython.org/ipython-doc/3/interactive/tutorial.html
+# https://docs.microsoft.com/en-us/visualstudio/python/interactive-repl-ipython
+# need ipython to support ?
+# skip it at first
+# % %matplotlib inline
+
+class kNN():
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        self.data = X
+        self.targets = y
+
+    def euclidean_distance(self, X):
+        """
+        Computes the euclidean distance between the training data and
+        a new input example or matrix of input examples X
+        """
+        # input: single data point
+        if X.ndim == 1:
+            l2 = np.sqrt(np.sum((self.data - X)**2, axis=1))
+
+        # input: matrix of data points
+        if X.ndim == 2:
+            n_samples, _ = X.shape
+            l2 = [np.sqrt(np.sum((self.data - X[i])**2, axis=1)) for i in range(n_samples)]
+
+        return np.array(l2)
+
+    def predict(self, X, k=1):
+        """
+        Predicts the classification for an input example or matrix of input examples X
+        """
+        # step 1: compute distance between input and training data
+        dists = self.euclidean_distance(X)
+
+        # step 2: find the k nearest neighbors and their classifications
+        if X.ndim == 1:
+            if k == 1:
+                nn = np.argmin(dists)
+                return self.targets[nn]
+            else:
+                knn = np.argsort(dists)[:k]
+                y_knn = self.targets[knn]
+                max_vote = max(y_knn, key=list(y_knn).count)
+                return max_vote
+
+        if X.ndim == 2:
+            knn = np.argsort(dists)[:, :k]
+            y_knn = self.targets[knn]
+            if k == 1:
+                return y_knn.T
+            else:
+                n_samples, _ = X.shape
+                max_votes = [max(y_knn[i], key=list(y_knn[i]).count) for i in range(n_samples)]
+                return max_votes
+
+
+# load dataset
+digits = load_digits()
+X, y = digits.data, digits.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+print(f'X_train shape: {X_train.shape}')
+print(f'y_train shape: {y_train.shape}')
+print(f'X_test shape: {X_test.shape}')
+print(f'y_test shape: {y_test.shape}')
+
+# Example digits
+# if plt install then you can use it
+'''
+fig = plt.figure(figsize=(10,8))
+for i in range(10):
+    ax = fig.add_subplot(2, 5, i+1)
+    plt.imshow(X[i].reshape((8,8)), cmap='gray')
+    
+plt.show()
+'''
+
+
+
+# initializing and training the model
+knn = kNN()
+knn.fit(X_train, y_train)
+
+print("Testing one datapoint, k=1")
+print(f"Predicted label: {knn.predict(X_test[0], k=1)}")
+print(f"True label: {y_test[0]}")
+print()
+print("Testing one datapoint, k=5")
+print(f"Predicted label: {knn.predict(X_test[20], k=5)}")
+print(f"True label: {y_test[20]}")
+print()
+print("Testing 10 datapoint, k=1")
+print(f"Predicted labels: {knn.predict(X_test[5:15], k=1)}")
+print(f"True labels: {y_test[5:15]}")
+print()
+print("Testing 10 datapoint, k=4")
+print(f"Predicted labels: {knn.predict(X_test[5:15], k=4)}")
+print(f"True labels: {y_test[5:15]}")
+print()
+
+
+# accuracy on test set
+# Compute accuracy on test set
+y_p_test1 = knn.predict(X_test, k=1)
+test_acc1= np.sum(y_p_test1[0] == y_test)/len(y_p_test1[0]) * 100
+print(f"Test accuracy with k = 1: {format(test_acc1)}")
+
+y_p_test5 = knn.predict(X_test, k=5)
+test_acc5= np.sum(y_p_test5 == y_test)/len(y_p_test5) * 100
+print(f"Test accuracy with k = 5: {format(test_acc5)}")
