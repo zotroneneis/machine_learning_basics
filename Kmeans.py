b406bccc3d32b31ff4255b78b09b799770f3554a
Chieh-Chun Chang <say543@msn.com>
2018-10-19 16:50:10 -0700

    Create Kmeans.py

---
+++ Kmeans.py
@@ -0,0 +1,74 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from sklearn.datasets import make_blobs
+np.random.seed(123)
+
+% matplotlib inline
+
+
+class KMeans():
+    def __init__(self, n_clusters=4):
+        self.k = n_clusters
+
+    def fit(self, data):
+        """
+        Fits the k-means model to the given dataset
+        """
+        n_samples, _ = data.shape
+        # initialize cluster centers
+        self.centers = np.array(random.sample(list(data), self.k))
+        self.initial_centers = np.copy(self.centers)
+
+        # We will keep track of whether the assignment of data points
+        # to the clusters has changed. If it stops changing, we are 
+        # done fitting the model
+        old_assigns = None
+        n_iters = 0
+
+        while True:
+            new_assigns = [self.classify(datapoint) for datapoint in data]
+
+            if new_assigns == old_assigns:
+                print(f"Training finished after {n_iters} iterations!")
+                return
+
+            old_assigns = new_assigns
+            n_iters += 1
+
+            # recalculate centers
+            for id_ in range(self.k):
+                points_idx = np.where(np.array(new_assigns) == id_)
+                datapoints = data[points_idx]
+                self.centers[id_] = datapoints.mean(axis=0)
+
+    def l2_distance(self, datapoint):
+        dists = np.sqrt(np.sum((self.centers - datapoint)**2, axis=1))
+        return dists
+
+    def classify(self, datapoint):
+        """
+        Given a datapoint, compute the cluster closest to the
+        datapoint. Return the cluster ID of that cluster.
+        """
+        dists = self.l2_distance(datapoint)
+        return np.argmin(dists)
+
+    def plot_clusters(self, data):
+        plt.figure(figsize=(12,10))
+        plt.title("Initial centers in black, final centers in red")
+        plt.scatter(data[:, 0], data[:, 1], marker='.', c=y)
+        plt.scatter(self.centers[:, 0], self.centers[:,1], c='r')
+        plt.scatter(self.initial_centers[:, 0], self.initial_centers[:,1], c='k')
+        plt.show()
+        
+        
+# initial and fit model
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(X)
+
+
+# plot and find clsuter center
+kmeans.plot_clusters(X)
